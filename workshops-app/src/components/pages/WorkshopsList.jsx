e1512927360d3239f7aea892529a7d7624dc1bee
Prashanth Puranik <puranik@digdeeper.in>
2023-01-08 12:39:09 +0530

    fetched workshops

--- workshops-app/src/components/pages/WorkshopsList.jsx
+++ workshops-app/src/components/pages/WorkshopsList.jsx
@@ -1,4 +1,21 @@
+import { useEffect } from 'react';
+import { getWorkshops } from '../../services/workshops';
+
 const WorkshopsList = () => {
+    useEffect(
+        // 1st argument -> function to call
+        () => { 
+            const helper = async () => {
+                const workshops = await getWorkshops();
+                console.log( workshops ); // shows the array
+            }
+
+            helper();
+        },
+        // 2nd argument -> empty array means we need the function to be called when the component renders for the first time
+        []
+    );
+
     return (
         <div>Workshops List page</div>
     );
--- workshops-app/src/services/workshops.js
+++ workshops-app/src/services/workshops.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const getWorkshops = async () => {
+    // why "await"? -> because we need to wait for the response to come from the backend. When we use "await", we need to mark our function as "async"
     const response = await axios.get( `https://workshops-server.herokuapp.com/workshops` )
     return response.data;
 };
