938fb5611b0e381b658143312344c96a481042b0
Prashanth Puranik <puranik@digdeeper.in>
2023-01-15 12:09:59 +0530

    showing error message on error

--- workshops-app/src/components/pages/WorkshopsList.jsx
+++ workshops-app/src/components/pages/WorkshopsList.jsx
@@ -4,15 +4,21 @@ import { getWorkshops } from '../../services/workshops';
 const WorkshopsList = () => {
     const [ workshops, setWorkshops ] = useState( [] );
     const [ loading, setLoading ] = useState( true );
+    const [ error, setError ] = useState( null );
 
     useEffect(
         // 1st argument -> function to call
         () => { 
             const helper = async () => {
-                const workshops = await getWorkshops();
-                console.log( workshops ); // shows the array
-                setWorkshops( workshops );
-                setLoading( false );
+                try {
+                    const workshops = await getWorkshops();
+                    console.log( workshops ); // shows the array
+                    setWorkshops( workshops );
+                } catch( error ) {
+                    setError( error );
+                } finally {
+                    setLoading( false );
+                }
             };
 
             helper();
@@ -34,23 +40,34 @@ const WorkshopsList = () => {
                     </div>
                 )
             }
-            <div className="row">
-                {
-                    // generate an array of JSX elements (React elements) using map()
-                    // [ <div>...</div>, <div>...</div>, ..., <div>...</div> ]
-                    workshops.map( w => (
-                        <div className="col-12 col-md-3 col-lg-4 d-flex">
-                            <div className="card p-3 w-100 my-3">
-                                <img src={w.imageUrl} className="card-img-top" alt={w.name} />
-                                <div className="card-body">
-                                    <h5 className="card-title">{w.name}</h5>
-                                    <div className="card-text" dangerouslySetInnerHTML={ { __html: w.description } }></div>
+            {
+                error && (
+                    <div class="alert alert-danger" role="alert">
+                        {error.message}
+                    </div>
+                )
+            }
+            {
+                workshops.length !== 0 && (
+                    <div className="row">
+                        {
+                            // generate an array of JSX elements (React elements) using map()
+                            // [ <div>...</div>, <div>...</div>, ..., <div>...</div> ]
+                            workshops.map( w => (
+                                <div className="col-12 col-md-3 col-lg-4 d-flex">
+                                    <div className="card p-3 w-100 my-3">
+                                        <img src={w.imageUrl} className="card-img-top" alt={w.name} />
+                                        <div className="card-body">
+                                            <h5 className="card-title">{w.name}</h5>
+                                            <div className="card-text" dangerouslySetInnerHTML={ { __html: w.description } }></div>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                    ) )
-                }
-            </div>
+                            ) )
+                        }
+                    </div>
+                )
+            }
         </div>
 
     );
