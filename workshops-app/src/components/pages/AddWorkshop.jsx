95e0a65f6fba0a766364cc82dbdd58b962e3c70c
Prashanth Puranik <puranik@digdeeper.in>
2023-01-15 12:32:24 +0530

    UI for add workshop form

--- workshops-app/src/components/pages/AddWorkshop.jsx
+++ workshops-app/src/components/pages/AddWorkshop.jsx
@@ -3,7 +3,27 @@ const AddWorkshop = () => {
         <div>
             <h1>Add a workshop</h1>
             <hr />
-            
+            <form>
+                <div class="row mb-3">
+                    <label htmlFor="name" class="col-sm-2 col-form-label">Name</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="name" />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label htmlFor="description" class="col-sm-2 col-form-label">Description</label>
+                    <div class="col-sm-10">
+                        <textarea class="form-control" id="description"></textarea>
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <label htmlFor="imageUrl" class="col-sm-2 col-form-label">Image URL</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="imageUrl" />
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary">Add a workshop</button>
+            </form>
         </div>
     );
 }
