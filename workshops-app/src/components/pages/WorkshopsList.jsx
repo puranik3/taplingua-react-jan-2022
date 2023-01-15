85fa0745cc66d80156e8cd423c536b97f0ba5586
Prashanth Puranik <puranik@digdeeper.in>
2023-01-15 11:36:36 +0530

    rendering lists

--- workshops-app/src/components/pages/WorkshopsList.jsx
+++ workshops-app/src/components/pages/WorkshopsList.jsx
@@ -24,17 +24,22 @@ const WorkshopsList = () => {
             <h1>List of workshops</h1>
             <hr />
             <div className="row">
-                <div className="col-12 col-md-3 col-lg-4">
-                    <div className="card">
-                        <img src="https://images.unsplash.com/photo-1561154464-82e9adf32764?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <h6 className="card-subtitle mb-2 text-muted ">Card subtitle</h6>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            b5
+                {
+                    // generate an array of JSX element (React elements)
+                    workshops.map( w => (
+                        <div className="col-12 col-md-3 col-lg-4 d-flex">
+                            <div className="card p-3 w-100 my-3">
+                                <img src={w.imageUrl} className="card-img-top" alt={w.name} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{w.name}</h5>
+                                    <div className="card-text">
+                                        {w.description}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
+                    ) )
+                }
             </div>
         </div>
 
